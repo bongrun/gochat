f97b5009a468da5105745749067c4f807d735af0
jumper423 <jump.e.r@yandex.ru>
2017-09-11 00:42:05 +0300

    first

---
+++ backend/README.md
@@ -0,0 +1,11 @@
+go get github.com/astaxie/beego
+go get github.com/beego/bee
+go get gopkg.in/mgo.v2
+go get gopkg.in/mgo.v2/bson
+
+
+go get github.com/Masterminds/glide
+go get github.com/beego/bee
+glide install
+
+bee run
---
+++ backend/controllers/default.go
@@ -0,0 +1,16 @@
+package controllers
+
+import (
+	"github.com/astaxie/beego"
+)
+
+type MainController struct {
+	beego.Controller
+}
+
+func (c *MainController) Get() {
+	c.Data["Website12"] = "beego.me1212"
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.TplName = "index.tpl"
+}
